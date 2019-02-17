7defe5d977adc7ead94826800bbd4b1bd1ded60b
Andrew Philpot <andrew.philpot@aofl.com>
2019-02-16 16:54:01 -0800

    s3pickle like s3json

---
+++ src/s3pickle.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+
+import pickle
+import boto3
+
+client = boto3.client('s3')
+
+
+class S3Pickle(object):
+    def __init__(self,
+                 bucket=None,
+                 key=None,
+                 uri=None):
+
+        if uri:
+            # TBD compute bucket and key
+            # TBD warn and ignore if bucket and/or key provided
+            pass
+        else:
+            self.bucket = bucket
+            self.key = key
+
+    def get(self):
+        response = client.get_object(Bucket=self.bucket, Key=self.key)
+        body_string = response['Body'].read()
+        deserialized = pickle.loads(body_string)
+        return deserialized
+
+    def put(self, content):
+        return None
